153cf8a02db6e9b6ab06c492ad813f694b048e58
Girish shrestha <gaggsghhshs@gmail.com>
2025-11-19 13:42:03 +0545

    remaining

---
+++ LiteralPractice.java
@@ -0,0 +1,12 @@
+public class LiteralPractice {
+    public static void main(String[] args) {
+
+        long bigNumber = 500000L;     
+        float decimalValue = 10.55f;      
+        char symbol = '\uDC99';          
+
+        System.out.println("Long value: " + bigNumber);
+        System.out.println("Float value: " + decimalValue);
+        System.out.println("Unicode char value: " + symbol);
+    }
+}
